fa858a4f500cd55de0e1ce75a095bbb872d24758
Lendynyte <Dom1n1c@seznam.cz>
2014-12-08 19:38:17 +0100

    added calculator

---
+++ Pauli_kalkulacka/Pauli_kalkulacka/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Pauli_kalkulacka
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.tBoxDisplay = new System.Windows.Forms.TextBox();
+      this.tlacitko_1 = new System.Windows.Forms.Button();
+      this.tlacitko_clear = new System.Windows.Forms.Button();
+      this.tlacitko_2 = new System.Windows.Forms.Button();
+      this.tlacitko_3 = new System.Windows.Forms.Button();
+      this.tlacitko_4 = new System.Windows.Forms.Button();
+      this.tlacitko_5 = new System.Windows.Forms.Button();
+      this.tlacitko_6 = new System.Windows.Forms.Button();
+      this.tlacitko_7 = new System.Windows.Forms.Button();
+      this.tlacitko_8 = new System.Windows.Forms.Button();
+      this.tlacitko_9 = new System.Windows.Forms.Button();
+      this.tlacitko_0 = new System.Windows.Forms.Button();
+      this.tlacitko_plus = new System.Windows.Forms.Button();
+      this.tlacitko_rovnase = new System.Windows.Forms.Button();
+      this.tlacitko_minus = new System.Windows.Forms.Button();
+      this.tlacitko_krat = new System.Windows.Forms.Button();
+      this.tlacitko_deleno = new System.Windows.Forms.Button();
+      this.tlacitko_odmocnina = new System.Windows.Forms.Button();
+      this.tlacitko_carka = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // tBoxDisplay
+      // 
+      resources.ApplyResources(this.tBoxDisplay, "tBoxDisplay");
+      this.tBoxDisplay.Name = "tBoxDisplay";
+      // 
+      // tlacitko_1
+      // 
+      resources.ApplyResources(this.tlacitko_1, "tlacitko_1");
+      this.tlacitko_1.Name = "tlacitko_1";
+      this.tlacitko_1.UseVisualStyleBackColor = true;
+      this.tlacitko_1.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_clear
+      // 
+      resources.ApplyResources(this.tlacitko_clear, "tlacitko_clear");
+      this.tlacitko_clear.Name = "tlacitko_clear";
+      this.tlacitko_clear.UseVisualStyleBackColor = true;
+      this.tlacitko_clear.Click += new System.EventHandler(this.tlacitko_clear_Click);
+      // 
+      // tlacitko_2
+      // 
+      resources.ApplyResources(this.tlacitko_2, "tlacitko_2");
+      this.tlacitko_2.Name = "tlacitko_2";
+      this.tlacitko_2.UseVisualStyleBackColor = true;
+      this.tlacitko_2.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_3
+      // 
+      resources.ApplyResources(this.tlacitko_3, "tlacitko_3");
+      this.tlacitko_3.Name = "tlacitko_3";
+      this.tlacitko_3.UseVisualStyleBackColor = true;
+      this.tlacitko_3.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_4
+      // 
+      resources.ApplyResources(this.tlacitko_4, "tlacitko_4");
+      this.tlacitko_4.Name = "tlacitko_4";
+      this.tlacitko_4.UseVisualStyleBackColor = true;
+      this.tlacitko_4.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_5
+      // 
+      resources.ApplyResources(this.tlacitko_5, "tlacitko_5");
+      this.tlacitko_5.Name = "tlacitko_5";
+      this.tlacitko_5.UseVisualStyleBackColor = true;
+      this.tlacitko_5.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_6
+      // 
+      resources.ApplyResources(this.tlacitko_6, "tlacitko_6");
+      this.tlacitko_6.Name = "tlacitko_6";
+      this.tlacitko_6.UseVisualStyleBackColor = true;
+      this.tlacitko_6.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_7
+      // 
+      resources.ApplyResources(this.tlacitko_7, "tlacitko_7");
+      this.tlacitko_7.Name = "tlacitko_7";
+      this.tlacitko_7.UseVisualStyleBackColor = true;
+      this.tlacitko_7.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_8
+      // 
+      resources.ApplyResources(this.tlacitko_8, "tlacitko_8");
+      this.tlacitko_8.Name = "tlacitko_8";
+      this.tlacitko_8.UseVisualStyleBackColor = true;
+      this.tlacitko_8.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_9
+      // 
+      resources.ApplyResources(this.tlacitko_9, "tlacitko_9");
+      this.tlacitko_9.Name = "tlacitko_9";
+      this.tlacitko_9.UseVisualStyleBackColor = true;
+      this.tlacitko_9.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_0
+      // 
+      resources.ApplyResources(this.tlacitko_0, "tlacitko_0");
+      this.tlacitko_0.Name = "tlacitko_0";
+      this.tlacitko_0.UseVisualStyleBackColor = true;
+      this.tlacitko_0.Click += new System.EventHandler(this.tlacitko_Click);
+      // 
+      // tlacitko_plus
+      // 
+      resources.ApplyResources(this.tlacitko_plus, "tlacitko_plus");
+      this.tlacitko_plus.Name = "tlacitko_plus";
+      this.tlacitko_plus.UseVisualStyleBackColor = true;
+      this.tlacitko_plus.Click += new System.EventHandler(this.tlacitkoZnamenka_Click);
+      // 
+      // tlacitko_rovnase
+      // 
+      resources.ApplyResources(this.tlacitko_rovnase, "tlacitko_rovnase");
+      this.tlacitko_rovnase.Name = "tlacitko_rovnase";
+      this.tlacitko_rovnase.UseVisualStyleBackColor = true;
+      this.tlacitko_rovnase.Click += new System.EventHandler(this.tlacitko_rovnase_Click);
+      // 
+      // tlacitko_minus
+      // 
+      resources.ApplyResources(this.tlacitko_minus, "tlacitko_minus");
+      this.tlacitko_minus.Name = "tlacitko_minus";
+      this.tlacitko_minus.UseVisualStyleBackColor = true;
+      this.tlacitko_minus.Click += new System.EventHandler(this.tlacitkoZnamenka_Click);
+      // 
+      // tlacitko_krat
+      // 
+      resources.ApplyResources(this.tlacitko_krat, "tlacitko_krat");
+      this.tlacitko_krat.Name = "tlacitko_krat";
+      this.tlacitko_krat.UseVisualStyleBackColor = true;
+      this.tlacitko_krat.Click += new System.EventHandler(this.tlacitkoZnamenka_Click);
+      // 
+      // tlacitko_deleno
+      // 
+      resources.ApplyResources(this.tlacitko_deleno, "tlacitko_deleno");
+      this.tlacitko_deleno.Name = "tlacitko_deleno";
+      this.tlacitko_deleno.UseVisualStyleBackColor = true;
+      this.tlacitko_deleno.Click += new System.EventHandler(this.tlacitkoZnamenka_Click);
+      // 
+      // tlacitko_odmocnina
+      // 
+      this.tlacitko_odmocnina.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+      resources.ApplyResources(this.tlacitko_odmocnina, "tlacitko_odmocnina");
+      this.tlacitko_odmocnina.Name = "tlacitko_odmocnina";
+      this.tlacitko_odmocnina.UseVisualStyleBackColor = false;
+      this.tlacitko_odmocnina.Click += new System.EventHandler(this.tlacitko_odmocnina_Click);
+      // 
+      // tlacitko_carka
+      // 
+      resources.ApplyResources(this.tlacitko_carka, "tlacitko_carka");
+      this.tlacitko_carka.Name = "tlacitko_carka";
+      this.tlacitko_carka.UseVisualStyleBackColor = true;
+      this.tlacitko_carka.Click += new System.EventHandler(this.tlacitko_carka_Click);
+      // 
+      // Form1
+      // 
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ButtonFace;
+      this.Controls.Add(this.tlacitko_carka);
+      this.Controls.Add(this.tlacitko_odmocnina);
+      this.Controls.Add(this.tlacitko_deleno);
+      this.Controls.Add(this.tlacitko_krat);
+      this.Controls.Add(this.tlacitko_minus);
+      this.Controls.Add(this.tlacitko_rovnase);
+      this.Controls.Add(this.tlacitko_plus);
+      this.Controls.Add(this.tlacitko_0);
+      this.Controls.Add(this.tlacitko_9);
+      this.Controls.Add(this.tlacitko_8);
+      this.Controls.Add(this.tlacitko_7);
+      this.Controls.Add(this.tlacitko_6);
+      this.Controls.Add(this.tlacitko_5);
+      this.Controls.Add(this.tlacitko_4);
+      this.Controls.Add(this.tlacitko_3);
+      this.Controls.Add(this.tlacitko_2);
+      this.Controls.Add(this.tlacitko_clear);
+      this.Controls.Add(this.tlacitko_1);
+      this.Controls.Add(this.tBoxDisplay);
+      this.Name = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox tBoxDisplay;
+    private System.Windows.Forms.Button tlacitko_1;
+    private System.Windows.Forms.Button tlacitko_clear;
+    private System.Windows.Forms.Button tlacitko_2;
+    private System.Windows.Forms.Button tlacitko_3;
+    private System.Windows.Forms.Button tlacitko_4;
+    private System.Windows.Forms.Button tlacitko_5;
+    private System.Windows.Forms.Button tlacitko_6;
+    private System.Windows.Forms.Button tlacitko_7;
+    private System.Windows.Forms.Button tlacitko_8;
+    private System.Windows.Forms.Button tlacitko_9;
+    private System.Windows.Forms.Button tlacitko_0;
+    private System.Windows.Forms.Button tlacitko_plus;
+    private System.Windows.Forms.Button tlacitko_rovnase;
+    private System.Windows.Forms.Button tlacitko_minus;
+    private System.Windows.Forms.Button tlacitko_krat;
+    private System.Windows.Forms.Button tlacitko_deleno;
+    private System.Windows.Forms.Button tlacitko_odmocnina;
+    private System.Windows.Forms.Button tlacitko_carka;
+  }
+}
+
---
+++ Pauli_kalkulacka/Pauli_kalkulacka/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pauli_kalkulacka
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+/*
+    private void tlacitko_1_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text +"1";
+    }*/
+
+    private void tlacitko_clear_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = string.Empty;
+      //tBoxDisplay.Text = "0";
+    }
+
+    private void tlacitko_Click(object sender, EventArgs e)
+    {
+      Button btn = sender as Button;
+      if (btn == null)
+        return;
+      if (jePrvni == true)
+      {
+        tBoxDisplay.Text = String.Empty;
+        tBoxDisplay.Text = tBoxDisplay.Text + btn.Text;
+        jePrvni = false;
+      }
+      else {
+        tBoxDisplay.Text = tBoxDisplay.Text + btn.Text;
+      }
+    }
+/*
+    private void tlacitko_2_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "2";
+    }
+
+    private void tlacitko_3_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "3";
+    }
+
+    private void tlacitko_4_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "4";
+    }
+
+    private void tlacitko_5_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "5";
+    }
+
+    private void tlacitko_6_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "6";
+    }
+
+    private void tlacitko_7_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "7";
+    }
+
+    private void tlacitko_8_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "8";
+    }
+
+    private void tlacitko_9_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "9";
+    }
+
+    private void tlacitko_0_Click(object sender, EventArgs e)
+    {
+      tBoxDisplay.Text = tBoxDisplay.Text + "0";
+    }*/
+
+  /*  private float vysledekPlus = 0;
+    private float vysledekMinus = 0;
+    private float vysledekKrat = 0;
+    private float vysledekDeleno = 0;*/
+    private float vysledek = 0;
+    private string posledniZmacknuteTlacitko;
+    private bool jeCarka = false;
+    private bool jePrvni = true;
+/*
+    private void tlacitko_plus_Click(object sender, EventArgs e)
+    {
+      //tBoxDisplay.Text = tBoxDisplay.Text + "+";
+      vysledekPlus = float.Parse(tBoxDisplay.Text);
+      tBoxDisplay.Text = string.Empty;
+      posledniZmacknuteTlacitko = "+";
+      jeCarka = false;
+    }*/
+    
+    private void tlacitko_rovnase_Click(object sender, EventArgs e)
+    {
+     /* int vysledek = int.Parse(tBoxDisplay.Text);
+      tBoxDisplay.Text = vysledek.ToString();*/
+      if (posledniZmacknuteTlacitko == "+")
+      {
+        tBoxDisplay.Text = (vysledek + float.Parse(tBoxDisplay.Text)).ToString();
+        vysledek = 0;
+
+      }
+      if (posledniZmacknuteTlacitko == "-")
+      {
+        tBoxDisplay.Text = (vysledek - float.Parse(tBoxDisplay.Text)).ToString();
+        vysledek = 0;
+      }
+      if (posledniZmacknuteTlacitko == "X")
+      {
+        tBoxDisplay.Text = (vysledek * float.Parse(tBoxDisplay.Text)).ToString();
+        vysledek = 0;
+      }
+      if (posledniZmacknuteTlacitko == ":")
+      {
+        float vysledekDelenoDva = float.Parse(tBoxDisplay.Text);
+        if (vysledekDelenoDva == 0)
+        {
+          tBoxDisplay.Text = "Nelze Delit Nulou";
+        }
+        else
+        {
+          tBoxDisplay.Text = (vysledek / vysledekDelenoDva).ToString();
+          vysledek = 0;
+          vysledekDelenoDva = 0;
+        }
+      }
+      posledniZmacknuteTlacitko = string.Empty;
+      jePrvni = true;
+    }
+
+    
+    private void tlacitkoZnamenka_Click(object sender, EventArgs e) {
+      if (float.TryParse(tBoxDisplay.Text, out vysledek) == false) {
+        vysledek = 0;
+      }
+      //float.TryParse(tBoxDisplay.Text, vysledek);
+      //tBoxDisplay.Text = string.Empty;
+      
+      Button btn = sender as Button;
+      if (btn == null)
+        return;
+      if (btn.Text == "+") {
+        posledniZmacknuteTlacitko = "+";
+      }
+      else if (btn.Text == "-") {
+        posledniZmacknuteTlacitko = "-";
+      }
+      else if (btn.Text == "X")
+      {
+        posledniZmacknuteTlacitko = "X";
+      }
+      else if (btn.Text == ":")
+      {
+        posledniZmacknuteTlacitko = ":";
+      }
+      jeCarka = false;
+      jePrvni = true;
+    }
+
+    /*
+    private void tlacitko_minus_Click(object sender, EventArgs e)
+    {
+      vysledekMinus = float.Parse(tBoxDisplay.Text);
+      tBoxDisplay.Text = string.Empty;
+      posledniZmacknuteTlacitko = "-";
+      jeCarka = false;
+    }
+
+    private void tlacitko_krat_Click(object sender, EventArgs e)
+    {
+      vysledekKrat = float.Parse(tBoxDisplay.Text);
+      tBoxDisplay.Text = string.Empty;
+      posledniZmacknuteTlacitko = "X";
+      jeCarka = false;
+    }
+
+    private void tlacitko_deleno_Click(object sender, EventArgs e)
+    {
+      vysledekDeleno = float.Parse(tBoxDisplay.Text);
+      tBoxDisplay.Text = string.Empty;
+      posledniZmacknuteTlacitko = ":";
+      jeCarka = false;
+    }*/
+
+    private void tlacitko_odmocnina_Click(object sender, EventArgs e)
+    {
+      float vysledekOdmocnina = float.Parse(tBoxDisplay.Text);
+      tBoxDisplay.Text = (Math.Sqrt(vysledekOdmocnina)).ToString();
+    }
+
+    private void tlacitko_carka_Click(object sender, EventArgs e)
+    {
+      if (jePrvni == false)
+      {
+        if (jeCarka == false)
+        {
+          tBoxDisplay.Text = tBoxDisplay.Text + ",";
+          jeCarka = true;
+        }
+      }
+    }
+
+  }
+}
